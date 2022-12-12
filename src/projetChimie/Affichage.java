70ee5f71e1ec549a9e2afe5337928000848adcc2
Nojul <120422228+Nojul@users.noreply.github.com>
2022-12-12 17:43:25 -0500

    Update Affichage.java

--- src/projetChimie/Affichage.java
+++ src/projetChimie/Affichage.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 /**
  * @author merie
- *
+ * @author noé
  */
 public class Affichage {
 		public Affichage() {
@@ -17,21 +17,29 @@ public class Affichage {
 		private void initialize() {
 			
 			Scanner input = new Scanner(System.in);
-	
+
 			PeriodicTable pt = new PeriodicTable();
 			CalculMole calcul = new CalculMole();
 			
+			//demande le nombre de cation
 			System.out.print( "Quel est votre cation?");
 			String cations = input.nextLine();
 			
+			//demande le nombre de cations
 			System.out.print("Combien avez vous de cation?");
 			double nb_cation  = input.nextDouble();
 			
+			//demande la charge de cation
 			System.out.print("Quel est la charge de votre cation?");
 			int nb_charge = input.nextInt();
+			
+			//met la charge sur le cation
 			pt.forSymbol(cations).setCharge(nb_charge);
 			
+			//trouve le nombre d'electrons
 			int nb_electron = pt.forSymbol(cations).getElectrons();
+			
+			//fait les calculs
 			double nx = calculMole(nb_cation, nb_electron);
 			double Nx = calcul.getNumberOf(nx);
 
