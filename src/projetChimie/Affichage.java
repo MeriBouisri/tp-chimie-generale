4c9c0536481313f31b48a6a008f6cf4eb586cb92
Nojul <120422228+Nojul@users.noreply.github.com>
2022-12-12 17:36:15 -0500

    Update Affichage.java

--- src/projetChimie/Affichage.java
+++ src/projetChimie/Affichage.java
@@ -21,17 +21,21 @@ public class Affichage {
 			PeriodicTable pt = new PeriodicTable();
 			CalculMole calcul = new CalculMole();
 			
-			
 			System.out.print( "Quel est votre cation?");
 			String cations = input.nextLine();
+			
+			System.out.print("Quel est le nombre de cations?");
+			double nb_cation  = input.nextDouble();
 			
 			System.out.print("Quel est la charge de votre cation?");
 			int nb_charge = input.nextInt();
 			pt.forSymbol(cations).setCharge(nb_charge);
 			
-			
 			int nb_electron = pt.forSymbol(cations).getElectrons();
-			Double Nx = calcul.getNumberOf(nb_electron);
+			double nx = (nb_cation* nb_electron);
+			double Nx = calcul.getNumberOf(nx);
+
+			System.out.print("Le nombre d'électrons dans " + nb_cation  + " ions "+ cations + "est de  "+ Nx);
 		}
 		
 }
