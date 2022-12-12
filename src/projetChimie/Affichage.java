a20f8d5585bf428ae3cbfda5d9ae811d36d90615
MeriBouisri <meriemebouisri@gmail.com>
2022-12-12 17:26:00 -0500

    added constructor

--- src/projetChimie/Affichage.java
+++ src/projetChimie/Affichage.java
@@ -3,24 +3,35 @@
  */
 package projetChimie;
 
+import java.util.Scanner;
+
 /**
  * @author merie
  *
  */
 public class Affichage {
-  Scanner input = new Scanner(System.in);
-
-		PeriodicTable pt = new PeriodicTable();
+		public Affichage() {
+			initialize();
+		}
 		
+		private void initialize() {
+			
+			Scanner input = new Scanner(System.in);
+	
+			PeriodicTable pt = new PeriodicTable();
+			CalculMole calcul = new CalculMole();
+			
+			
+			System.out.print( "Quel est votre cation?");
+			String cations = input.nextLine();
+			
+			System.out.print("Quel est la charge de votre cation?");
+			int nb_charge = input.nextInt();
+			pt.forSymbol(cations).setCharge(nb_charge);
+			
+			
+			int nb_electron = pt.forSymbol(cations).getElectrons();
+			Double Nx = calcul.getNumberOf(nb_electron);
+		}
 		
-		System.out.print( "Quel est votre cation?");
-		String cations = input.nextLine();
-		
-		System.out.print("Quel est la charge de votre cation?");
-		int nb_charge = input.nextInt();
-		pt.forSymbol(cations).setCharge(nb_charge);
-		
-		
-		int nb_electron = pt.forSymbol(cations).getElectrons();
-		Double Nx = calcul.convertion(nb_electron);
 }
