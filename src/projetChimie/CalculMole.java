a4a3dc0696df2157ad293009c528c2cb4b4e0126
MeriBouisri <meriemebouisri@gmail.com>
2022-12-12 17:21:19 -0500

    Added calculMole method

--- src/projetChimie/CalculMole.java
+++ src/projetChimie/CalculMole.java
@@ -4,12 +4,15 @@ public class CalculMole {
 
 	private final double CONST_AVOGADRO = 6.022 * (Math.pow(10, 23));
 	
-	public double getNumberOf(int baseUnit) {
+	public double getNumberOf(double baseUnit) {
 		return baseUnit * CONST_AVOGADRO;
 	}
 	
-	public double getMoleOf(int numberUnit) {
+	public double getMoleOf(double numberUnit) {
 		return numberUnit / CONST_AVOGADRO;
 	}
 	
+	public double calculMole(double numberUnit, int molElectron) {
+		return numberUnit * molElectron;
+	}
 }
